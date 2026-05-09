21623eef3b293507686f68270a97248f95eac30d
zsh260439 <213618132+zsh260439@users.noreply.github.com>
2026-05-10 00:15:03 +0800

    重构-客户端-重命名防抖组合函数并统一工具导出

--- apps/client/src/utils/index.ts
+++ apps/client/src/utils/index.ts
@@ -3,4 +3,8 @@ export {
   sanitizeArticleFormPayload,
 } from './article'
 export { formatDate } from './formatDate'
+export {
+  createMarkdownHeadingId,
+  resolveMarkdownHeadingId,
+} from './markdown'
 export { createSlugFromText } from './slug'
